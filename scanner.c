a6b47f299d5b9f1a9a21d1bc74294ffefdcddfa1
landyacht <alexander.eubanks@protonmail.com>
2020-09-22 15:00:58 -0400

    Skeleton code for lexer

---
+++ scanner.c
@@ -0,0 +1,96 @@
+#include "scanner.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum lexer_state {
+	state_start,         /* no characters yet */
+	state_maybe_relop,   /* we have encountered < or > or / which may be alone or with an = */
+	state_def_relop,     /* we definitely have a relop */
+	state_ident_kw_op,   /* we have part or all of an identifier, keyword, or operator (and, or, xor, mod, rem) */
+	state_def_binop,     /* we definitely have a binary operator (+ - * &) */
+	state_maybe_assign,  /* we either have the assignment op or a colon */
+	state_in_str,        /* we're inside a string */
+	state_num_leftside,  /* either an integer or the left side of a decimal literal */
+	state_num_dec,       /* we're at the decimal point in a decimal literal */
+	state_num_rightside, /* the right side of the decimal in a decimal literal */
+};
+
+FILE *fp;
+
+int lexer_init(char *filename) {
+	fp = fopen(filename, "r");
+	if (NULL == fp) {
+		sprintf(current_error, "fopen failed with errno = %d", errno);
+		return 1;
+	}
+
+	return 0;
+}
+
+int lexer_next() {
+	/* Where in current_lexeme we are */
+	static int needle = 0;
+	/* Have we reached the end of the file? */
+	static bool is_eof = 0;
+	/* Our current state, so we know how to transition */
+	enum lexer_state cur_state = state_start;
+
+	if (is_eof) {
+		return 1;
+	}
+
+	char read;
+	while (1) {
+		int num_read = fread(&read, 1, 1, fp);
+		/* Check for error or EOF marker if we didn't read a byte */
+		if (0 == num_read) {
+			if (ferror(fp)) {
+				sprintf(current_error, "fread failed");
+				return 2;
+			}
+
+			if (feof(fp)) {
+				is_eof = 1;
+			}
+
+			clearerr();
+		}
+
+		/* For convenience in the switch statement */
+		bool is_ws = is_eof
+			| read == ' '  /* space */
+			| read == '\t' /* tabulation */
+			| read == '\n' /* newline */
+			| read == '\r' /* carriage return */
+			;
+
+		/* TODO implement the state machine here */
+		switch (cur_state) {
+		case state_start:
+			break;
+		case state_maybe_relop:
+			break;
+		case state_def_relop:
+			break;
+		case state_ident_kw_op:
+			break;
+		case state_def_binop:
+			break;
+		case state_maybe_assign:
+			break;
+		case state_in_str:
+			break;
+		case state_num_leftside:
+			break;
+		case state_num_dec:
+			break;
+		case state_num_rightside:
+			break;
+		}
+	}
+
+	/* Theoretically unreachable code */
+	puts("This should never print!!!");
+}
---
+++ scanner.h
@@ -0,0 +1,64 @@
+#define MAX_LEXEME_LEN 150
+#define MAX_ERROR_LEN 100
+
+/* Associate named token types with integer values */
+enum token_type {
+	token_type_keyword_proc,         /* "procedure" */
+	token_type_keyword_is,           /* "is" */
+	token_type_keyword_begin,        /* "begin" */
+	token_type_keyword_end,          /* "end" */
+	token_type_colon,                /* Colon for declarations, e.g. Foo: Integer */
+	token_type_identifier,           /* Identifiers such as variable & procedure names, type names */
+	token_type_lit_int,              /* Integer literal */
+	token_type_lit_dec,              /* Decimal (floating point) literal */
+	token_type_lit_str,              /* String literal */
+	/* The following operators listed in order of decreasing precedence */
+	token_type_unop_not              /* Logical negation (not) */
+	token_type_binop_multiplicative, /* Binary operators with multiplicative precedence (* / mod rem) */
+	token_type_binop_additive,       /* Binary operators with additive precedence (+ -) */
+	token_type_binop_relational,     /* Relational operators like =, >, <, etc. */
+	token_type_binop_logical,        /* Logical operators like and, or, xor */
+	token_type_binop_assign,         /* := */
+	/* End operator listing */
+	token_type_statement_sep  /* Semicolons separating statements */
+};
+
+/* 
+ * Union type for possible lexeme types 
+ * If the lexeme is a string, val_str shall point to a heap-allocated copy of the string contents
+ * This will reduce the amount of work for the parser in building the symbol table
+ */
+union lexeme_value {
+	long    val_int;
+	double  val_dec;
+	char   *val_str;
+};
+
+/* Globals to store current values after a call to next_lexeme */
+char current_lexeme[MAX_LEXEME_LEN + 1]; /* 1 byte for null terminator */
+enum token_type current_token_type;
+union lexeme_value current_value;
+char current_error[MAX_ERROR_LEN + 1]; /* 1 byte for null terminator */
+
+/*
+ * lexer_init - Initialize the lexer (must be called before any other lexer subroutines)
+ * Parameters:
+ *   filename - Path to file to be lexed
+ * Returns:
+ *   0 - Success
+ *   1 - Failed to open file (current_error will be set)
+ */
+int lexer_init(char *filename);
+
+/*
+ * lexer_next - Read a lexeme and put relevant info in globals
+ * Returns:
+ *   0 - Success
+ *   1 - Reached end of file (no lexeme to report)
+ *   2 - Failed to read (current_error will be set)
+ *   3 - Invalid token (current_error will be set)
+ */
+int lexer_next();
+
+/* lexer_close - Release resources and return to un-initialized state */
+void lexer_close();
